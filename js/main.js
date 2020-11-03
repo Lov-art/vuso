8df68ce5fcdcad646b19deb55d288db00418d496
Lovin Artem <lyovinn.artem@gmail.com>
2020-11-03 14:39:43 +0200

    first2

--- js/main.js
+++ js/main.js
@@ -37,32 +37,32 @@
 // });
 
 // INPUT RANGE
-
+const rng = document.getElementById("r1"); //это Input
+var p = document.getElementById("one"); // p - абзац
+var price = document.getElementById("user-policy-price"); // Стоймость полиса(позже исправить!!!!!!!)
+var assistance = document.getElementById('assistance');
+var insured = document.querySelectorAll('.insured-sum');
 function fun1() {
-	var rng = document.getElementById("r1"); //это Input
-	var p = document.getElementById("one"); // p - абзац
-	var price = document.getElementById("user-policy-price"); // Стоймость полиса(позже исправить!!!!!!!)
-	var assistance = document.getElementById('assistance');
-	var insured = document.querySelectorAll('.insured-sum');
 	p.innerHTML = rng.value;
 	price.innerHTML = Math.round(rng.value / 245);
-	insured.innerHTML = rng.value;
+	// insured.innerHTML = rng.value;
 	for (let i = 0; i < insured.length; i++) {
 		insured[i].innerHTML = rng.value;
 	}
-	rng.addEventListener('mousemove', function () {
-		var x = rng.value * 100 / 3000000;
-		var color = 'linear-gradient(90deg,rgb(156, 170, 15)' + x + '%,rgb(214, 214, 214)' + x + '%)'
-		rng.style.background = color;
-		console.log(color);
-	})
 
 }
-fun1()
+function fun2() {
+	p.innerHTML = rng.value;
+	for (let i = 0; i < insured.length; i++) {
+		insured[i].innerHTML = rng.value;
+	}
 
-
-
-
+}
+rng.addEventListener('mousemove', function () {
+	var x = rng.value * 100 / 3000000;
+	var color = 'linear-gradient(90deg,rgb(156, 170, 15)' + x + '%,rgb(214, 214, 214)' + x + '%)'
+	rng.style.background = color;
+})
 // БЛОК С ВОПРОСАМИ
 $('.questions-btn').on('click', function () {
 	var $btn = $(this);
@@ -75,21 +75,12 @@ $('.questions-btn').on('click', function () {
 	}
 })
 // БЛОК С ВОПРОСАМИ
-
-
-
-
 // ПОДСКАЗКА
 $(function () {
 	$('[data-toggle="popover"]').popover();
 });
 // ПОДСКАЗКА
-
-
-
-
 // Счетчик
-
 function handleTickInit(tick) {
 
 	// set final value, tick will animate towards it
@@ -98,7 +89,6 @@ function handleTickInit(tick) {
 
 	}, 0.01);
 }
-
 function handleTickInitSecond(tick) {
 
 	// set final value, tick will animate towards it
@@ -114,4 +104,98 @@ function handleTickInitThird(tick) {
 		tick.value = 4321749;
 
 	}, 0.01);
+}
+///calc-step-two
+let select = document.querySelector('.calc-select');
+let brand = document.getElementById('brand');
+let year = document.getElementById('year');
+let city = document.getElementById('city');
+let drivers = document.getElementById('drivers');
+let deterioration = document.getElementById('deterioration');
+let repair = document.getElementById('repair');
+let payment = document.getElementById('payment');
+let credit = document.getElementById('credit');
+let taxi = document.getElementById('taxi');
+
+let userChoiceBrand = document.querySelectorAll('.user-choice-brand');
+let userChoiceYear = document.querySelectorAll('.user-choice-year');
+let userChoiceCity = document.querySelectorAll('.user-choice-city');
+let userChoiceDrivers = document.querySelectorAll('.user-choice-drivers');
+let userChoicePrice = document.querySelectorAll('.user-choice-price');
+let userChoiceDeterioration = document.querySelectorAll('.user-choice-deterioration');
+let userChoiceRepair = document.querySelectorAll('.user-choice-repair');
+let userChoicePayment = document.querySelectorAll('.user-choice-payment');
+let userChoiceCredit = document.querySelectorAll('.user-choice-credit');
+let userChoiceTaxi = document.querySelectorAll('.user-choice-taxi');
+
+let calcStepTwo = document.querySelector('.calc-step-two');
+let calcStepOne = document.querySelector('.calc-step-one');
+let calcStepThree = document.querySelector('.calc-step-3');
+let calcStepFour = document.querySelector('.calc-step-4');
+
+
+function stepChange() {
+	calcStepTwo.style.display = 'none';
+	calcStepOne.style.display = 'block';
+}
+function stepTwo() {
+	calcStepTwo.style.display = 'block';
+	calcStepOne.style.display = 'none';
+
+	for (let i = 0; i < userChoiceBrand.length; i++) {
+		userChoiceBrand[i].innerHTML = brand.value;
+	}
+
+	for (let i = 0; i < userChoiceYear.length; i++) {
+		userChoiceYear[i].innerHTML = year.value;
+	}
+
+	for (let i = 0; i < userChoiceDrivers.length; i++) {
+		userChoiceDrivers[i].innerHTML = drivers.value;
+	}
+
+	for (let i = 0; i < userChoiceCity.length; i++) {
+		userChoiceCity[i].innerHTML = city.value;
+	}
+
+	for (let i = 0; i < userChoicePrice.length; i++) {
+		userChoicePrice[i].innerHTML = rng.value + ' ' + 'грн';
+	}
+
+	for (let i = 0; i < userChoiceDeterioration.length; i++) {
+		userChoiceDeterioration[i].innerHTML = deterioration.value;
+	}
+
+	for (let i = 0; i < userChoiceRepair.length; i++) {
+		userChoiceRepair[i].innerHTML = repair.value;
+	}
+
+	for (let i = 0; i < userChoicePayment.length; i++) {
+		userChoicePayment[i].innerHTML = payment.value;
+	}
+
+	for (let i = 0; i < userChoiceCredit.length; i++) {
+		if (credit.checked) {
+			userChoiceCredit[i].innerHTML = 'кредит';
+		} else {
+			userChoiceCredit[i].innerHTML = 'не кредит';
+		};
+	}
+	for (let i = 0; i < userChoiceTaxi.length; i++) {
+		if (taxi.checked) {
+			userChoiceTaxi[i].innerHTML = 'такси';
+		} else {
+			userChoiceTaxi[i].innerHTML = 'не такси';
+		};
+	}
+
+
+}
+function stepThree() {
+	calcStepTwo.style.display = 'none';
+	calcStepThree.style.display = 'block';
+}
+function stepFour() {
+	calcStepThree.style.display = 'none';
+	calcStepFour.style.display = 'block';
 }
